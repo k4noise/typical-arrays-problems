62408bebbdc97e43f2cfad8de61e6fe5ab8c9491
Marina Kistanova <48287166+k4noise@users.noreply.github.com>
2021-01-05 19:57:30 +0500

    add: add solutions to typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = [0]) {
+  return array.length > 0 ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = [0]) {
+  return array.length > 0 ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = [0]) {
+  return array.length > 0 ? array.reduce((num, acc) => num + acc, 0) / array.length : 0;
 }
